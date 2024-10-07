f7feb49f38c3a42a8d22f23f8edffbd166bea56c
Raj-yadav-7822 <ry682734@gmail.com>
2024-10-07 19:36:52 +0530

    backend 1 class

---
+++ math.js
@@ -0,0 +1,13 @@
+const sum = (a,b)=>a+b;
+const mul = (a,b)=>a*b;
+const divd= (a,b)=>a%b;
+const g = 9.8;
+
+let obj={
+    sum:sum,
+    mul:mul,
+    divd:divd,
+    g:g
+}
+
+module.exports = obj;
---
+++ script.js
@@ -0,0 +1,5 @@
+const someValue = require("./math");
+
+console.log(someValue.mul(4,3));
+console.log(someValue.sum(4,3));
+console.log(someValue.divd(4,3));
